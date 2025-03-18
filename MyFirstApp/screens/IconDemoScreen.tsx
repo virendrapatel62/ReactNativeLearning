8e00dd39d0b645e4e54ac20769b0137f24dbc182
Virendra patel <patelvirendra62@gmail.com>
2025-03-18 22:11:21 +0530

    Added color picker options

--- MyFirstApp/screens/IconDemoScreen.tsx
+++ MyFirstApp/screens/IconDemoScreen.tsx
@@ -1,8 +1,10 @@
 import React, {Fragment, useState} from 'react';
+import Slider from '@react-native-community/slider';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import ColorWheel from 'react-native-wheel-color-picker';
 import Text from '../components/atoms/Text/Text.component';
 import Flexbox from '../components/organisms/Flexbox/Flexbox.component';
+import {ScrollView, StyleSheet, View} from 'react-native';
 
 const icons = [
   'facebook',
@@ -61,35 +63,74 @@ const icons = [
 
 export default function IconDemoScreen() {
   const [color, setColor] = useState('#000000');
+  const [iconSize, setIconSize] = useState(0); // Initial value of the slider
 
   const handleColorChange = (newColor: string) => {
     setColor(newColor);
   };
 
   return (
-    <Flexbox justifyContentCenter alignItemsCenter gap={20}>
+    <Flexbox style={styles.container}>
       <Flexbox
-        row
-        justifyContentCenter
         style={{
-          flexWrap: 'wrap',
-        }}
-        gap={16}
-        alignItemsCenter>
-        {icons.map(icon =>
-          Icon.hasIcon(icon) ? (
-            <Icon key={icon} name={icon} size={30} color={color} />
-          ) : (
-            <Fragment />
-          ),
-        )}
+          flex: 60,
+          margin: 10,
+        }}>
+        <Flexbox alignItemsCenter>
+          <Slider
+            style={styles.slider}
+            minimumValue={0}
+            maximumValue={50}
+            step={1}
+            value={iconSize}
+            onValueChange={newValue => setIconSize(newValue)}
+            minimumTrackTintColor="#FF5733" // Color of the track on the left side
+            maximumTrackTintColor="#CCCCCC" // Color of the track on the right side
+            thumbTintColor="#FF5733" // Color of the thumb (slider knob)
+          />
+        </Flexbox>
+
+        <ScrollView
+          style={{
+            borderRadius: 16,
+            borderWidth: 1,
+            padding: 16,
+            borderColor: color,
+          }}>
+          <Flexbox
+            justifyContentCenter
+            alignItemsCenter
+            style={{
+              marginTop: 20,
+            }}
+            gap={20}>
+            <Flexbox
+              row
+              justifyContentCenter
+              style={{
+                flexWrap: 'wrap',
+              }}
+              gap={16}
+              alignItemsCenter>
+              {icons.map(icon =>
+                Icon.hasIcon(icon) ? (
+                  <Icon key={icon} name={icon} size={iconSize} color={color} />
+                ) : (
+                  <Fragment />
+                ),
+              )}
+            </Flexbox>
+          </Flexbox>
+        </ScrollView>
       </Flexbox>
+
       <Text size={30} color={color} weight={600}>
         Current Color : {color}
       </Text>
+
       <Flexbox
         style={{
-          width: '90%',
+          height: 300,
           aspectRatio: 1,
           marginTop: 20,
         }}>
@@ -98,3 +139,17 @@ export default function IconDemoScreen() {
     </Flexbox>
   );
 }
+
+const styles = StyleSheet.create({
+  slider: {
+    width: 300,
+    height: 40,
+  },
+
+  container: {
+    flex: 1,
+    position: 'relative',
+    display: 'flex',
+    alignItems: 'center',
+  },
+});
