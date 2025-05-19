ea82e41cac38aac8412a5431b1f2b2e08a07fe27
Virendra patel <patelvirendra62@gmail.com>
2025-05-19 07:20:42 +0530

    Update FilePicker.screen.tsx

--- DailyTasks/screens/FilePicker.screen.tsx
+++ DailyTasks/screens/FilePicker.screen.tsx
@@ -5,23 +5,25 @@ import {pick} from '@react-native-documents/picker';
 
 export default async function FilePickerScreen() {
   return (
-    <View>
-      <Text fontSize={28} color="black">
-        File Picker
-      </Text>
 
-      <Button
-        title="single file import"
-        onPress={async () => {
-          try {
-            const [pickResult] = await pick();
-            // const [pickResult] = await pick({mode:'import'}) // equivalent
-            // do something with the picked file
-          } catch (err: unknown) {
-            // see error handling
-          }
-        }}
-      />
-    </View>
+     <Text>Hello</Text>
+    // <View>
+    //   <Text fontSize={28} color="black">
+    //     File Picker
+    //   </Text>
+
+    //   <Button
+    //     title="single file import"
+    //     onPress={async () => {
+    //       try {
+    //         const [pickResult] = await pick();
+    //         // const [pickResult] = await pick({mode:'import'}) // equivalent
+    //         // do something with the picked file
+    //       } catch (err: unknown) {
+    //         // see error handling
+    //       }
+    //     }}
+    //   />
+    // </View>
   );
 }
